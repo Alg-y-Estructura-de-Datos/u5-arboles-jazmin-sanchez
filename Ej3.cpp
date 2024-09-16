3e21d7f26d0ebb81e51aa7e9078eefe9500d2d50
jazminmorenasanchez <2321544@ucc.edu.ar>
2024-09-16 15:31:57 -0300

    trees

--- Ej3.cpp
+++ Ej3.cpp
@@ -1,4 +1,14 @@
+/*Sistema de inscripción en un gimnasio
+Problema: Estás diseñando un sistema de inscripción para un gimnasio, donde cada socio
+tiene un número de socio único. El sistema debe permitir la inscripción de nuevos socios, la
+eliminación de socios que se dan de baja, y la búsqueda de socios activos.
+● Tarea: Inscribe a los socios con los siguientes números de socio: 500, 250, 750, 100,
+300, 600, 800.
+● Desafío: Un socio con el número 250 se da de baja, así que elimínalo. Luego,
+verifica si los socios con los números 300 y 750 aún están activos. Imprime la lista
+de socios actualizada.*/
 #include <iostream>
+#include "Arbol/ArbolBinario.h"
 using namespace std;
 
 int main() {
