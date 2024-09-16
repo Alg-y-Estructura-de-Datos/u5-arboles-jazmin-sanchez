73a0624713bbe0c626a293fb17059869f3d1387a
jazminmorenasanchez <2321544@ucc.edu.ar>
2024-09-16 15:30:52 -0300

    arboles

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,44 @@
+/*: Estás organizando una biblioteca familiar. Cada libro tiene un número único de
+identificación (ID) y quieres organizar los libros en un sistema que te permita agregarlos,
+eliminarlos y buscarlos fácilmente.
+● Tarea: Inserta los libros con los siguientes IDs en el sistema de gestión (árbol): 101,
+52, 198, 36, 75, 150, 200.
+● Desafío: Busca el libro con el ID 75 y verifica si está en la biblioteca. Luego elimina
+el libro con el ID 52 porque fue prestado. Imprime la estructura del sistema después
+de la eliminación.*/
 #include <iostream>
-using namespace std;
+#include "Arbol/ArbolBinario.h"
 
 int main() {
+    // Crear el árbol binario
+    ArbolBinario<int> biblioteca;
 
-    cout << "Ejercicio N° 1" << endl;
+    // Insertar los libros en el sistema de gestión
+    biblioteca.put(101);
+    biblioteca.put(52);
+    biblioteca.put(198);
+    biblioteca.put(36);
+    biblioteca.put(75);
+    biblioteca.put(150);
+    biblioteca.put(200);
+
+    // Buscar el libro con ID 75
+    try {
+        int libro = biblioteca.search(75);
+        std::cout << "El libro con ID 75 está en la biblioteca: " << libro << std::endl;
+    } catch (int e) {
+        if (e == 404) {
+            std::cout << "El libro con ID 75 no está en la biblioteca." << std::endl;
+        }
+    }
+
+    // Eliminar el libro con ID 52 (prestado)
+    biblioteca.remove(52);
+    std::cout << "El libro con ID 52 ha sido eliminado (prestado)." << std::endl;
+
+    // Imprimir la estructura del árbol después de la eliminación
+    std::cout << "Estructura del sistema de libros después de eliminar el ID 52:" << std::endl;
+    biblioteca.print(); // Imprime el árbol en orden (en este caso, por IDs de libros)
 
     return 0;
 }
--- Ej2.cpp
+++ Ej2.cpp
@@ -1,9 +1,51 @@
+/*: En una tienda de electrónica, cada producto tiene un código único y quieres
+gestionar el inventario. A medida que entran y salen productos, es necesario mantener el
+sistema actualizado.
+● Tarea: Inserta los productos con los siguientes códigos: 4001, 2002, 6003, 1500,
+3500, 5000, 7000.
+● Desafío: Un cliente compró los productos con códigos 2002 y 6003, así que
+elimínalos del sistema. Luego, un nuevo producto con el código 2500 llega al
+inventario. Imprime el inventario actualizado y verifica si el código 4001 todavía está
+en el sistema.*/
 #include <iostream>
+#include "Arbol/ArbolBinario.h"
 using namespace std;
 
 int main() {
+    // Crear el árbol binario
+    ArbolBinario<int> tienda;
 
-    cout << "Ejercicio N° 2" << endl;
+    // Insertar los libros en el sistema de gestión
+    std::cout << "\nEstructura del sistema original : \n" << std::endl;
+    tienda.put(4001);
+    tienda.put(2002);
+    tienda.put(6003);
+    tienda.put(1500);
+    tienda.put(3500);
+    tienda.put(5000);
+    tienda.put(7000);
+
+    tienda.print();
+
+    tienda.remove(2002);
+    tienda.remove(6003);
+    std::cout << "\nse eliminaron dos productos, 6003 y 2002." << std::endl;
+    tienda.print();
+
+    std::cout << "\nSe agrega nuevo producto: (2500) \n" << std::endl;
+    tienda.put(2500);
+
+    try {
+        int producto = tienda.search(4001);
+        std::cout << "\nEl producto "<< producto <<" esta en la tienda: " << std::endl;
+    } catch (int e) {
+        if (e == 404) {
+            std::cout << "\nEl producto 4001 no está en la tienda." << std::endl;
+        }
+    }
+    // Imprimir la estructura del árbol después de la eliminación
+    std::cout << "\nEstructura del sistema: \n" << std::endl;
+    tienda.print(); // Imprime el árbol en orden (en este caso, por IDs de libros)
 
     return 0;
 }
